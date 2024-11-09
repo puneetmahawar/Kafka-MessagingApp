56d17fb6c3ce3c226391167d33b0e6dbf0638942
puneet <puneetmahawarnitmz@gmail.com>
2024-11-09 21:36:39 +0530

    Adding Project

---
+++ KafkaMessageApp/src/main/java/com/message/KafkaMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageAppApplication.class, args);
+	}
+
+}
